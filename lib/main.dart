8790edb985242e7a25dd62e5aa11b5a9b00f82eb
dfathu <dfathu@gmail.com>
2021-12-14 09:16:41 +0700

    update 12/14/2021

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,16 @@ import 'package:bwa_airplane/ui/pages/get_started_pages.dart';
 import 'package:bwa_airplane/ui/pages/main_page.dart';
 import 'package:bwa_airplane/ui/pages/sign_up_pages.dart';
 import 'package:bwa_airplane/ui/pages/transaction_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 // import 'ui/pages/Splash_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
