9a4bbe86ca7a9585ed175db7206f2b1cbc1abcce
dfathu <dfathu@gmail.com>
2021-12-21 07:44:13 +0700

    update home_page ui 12/21/2021

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bwa_airplane/shared/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,11 +6,82 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text('Home Page')],
-      ),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 30,
+          left: defaultMargin,
+          right: defaultMargin,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Howdy,\nKezia Anne',
+                    style: blackTextStyle.copyWith(
+                        fontSize: 24, fontWeight: semiBold),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    'Where to fly today?',
+                    style:
+                        greyTextStyle.copyWith(fontSize: 16, fontWeight: light),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/image_profile.png'))),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget popularDestinations() {
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        child: Row(
+          children: [
+            Container(
+              width: 200,
+              height: 323,
+              margin: EdgeInsets.only(left: defaultMargin),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18), color: kWhiteColor),
+              child: Column(
+                children: [
+                  Container(
+                    width: 180,
+                    height: 220,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18),
+                        image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/image_destination1.png'))),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      children: [header(), popularDestinations()],
     );
   }
 }
